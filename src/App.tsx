d0259335302fbd7939c5334344237bd830cf365a
Oyindamola Oyetunmibi <oyindadavid18@gmail.com>
2023-07-06 11:06:55 +0100

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import TopBar from "./components/TopBar";
 import TodoList from "./components/TodoList";
 import TodoAdd from "./components/TodoAdd";
 
-export default function App() {
+export function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box maxWidth="8xl" margin="auto" p={5}>
